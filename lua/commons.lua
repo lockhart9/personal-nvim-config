59a9abaa4832eec8659df4671df5951ff994b053
Shotaro Manago <shotaro-manago@c-fo.com>
2023-09-06 09:24:16 +0900

    add telescope live plugin

--- lua/commons.lua
+++ lua/commons.lua
@@ -7,7 +7,8 @@ vim.opt.smartcase = true
 vim.opt.ignorecase = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
-vim.opt.cursorcolumn = true
+vim.opt.conceallevel = 2
+-- vim.opt.cursorcolumn = true
 
 vim.keymap.set('n', 'Q', '<cmd>tabp<CR>', {noremap = true})
 vim.keymap.set('n', '<S-Tab>', '<cmd>tabn<CR>', {noremap = true})
@@ -25,4 +26,20 @@ vim.keymap.set('n', '<Leader>4', '<cmd>AsyncRun cargo run<CR>', {noremap = true}
 vim.keymap.set('n', '<Leader>5', '<cmd>AsyncRun cargo build<CR>', {noremap = true})
 vim.keymap.set('n', '<Leader>6', '<cmd>AsyncRun cargo build --release<CR>', {noremap = true})
 
-vim.cmd [[colorscheme habamax]]
+
+-- vim.cmd [[colorscheme habamax]]
+vim.cmd [[colorscheme desert]]
+
+vim.keymap.set('n', '<c-g>', '<CMD>diffget<CR>', {noremap = true})
+vim.keymap.set('n', '<c-p>', '<CMD>diffput<CR>', {noremap = true})
+
+
+-- markdown
+vim.g.vim_markdown_folding_disabled = 1
+
+vim.cmd([[
+    autocmd FileType markdown nmap <buffer><silent> <leader>p :call mdip#MarkdownClipboardImage()<CR>
+    " there are some defaults for image directory and image name, you can change them
+    let g:mdip_imgdir = 'img'
+    let g:mdip_imgname = 'image'
+]])
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,13 +7,23 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use 'nvim-lua/plenary.nvim'
-    use 'nvim-telescope/telescope.nvim'
+    -- use 'nvim-telescope/telescope.nvim'
+    use {
+        "nvim-telescope/telescope.nvim",
+        requires = {
+            { "nvim-telescope/telescope-live-grep-args.nvim" },
+        },
+        config = function()
+            require("telescope").load_extension("live_grep_args")
+        end
+    }
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     use 'nvim-telescope/telescope-file-browser.nvim'
     -- use 'renerocksai/telekasten.nvim'
     use 'dhruvasagar/vim-table-mode'
     use 'skywind3000/asyncrun.vim'
     use 'lockhart9/vim-grep-shortcut'
+    use 'img-paste-devs/img-paste.vim'
     use {
         'nvim-tree/nvim-tree.lua',
         requires = {
@@ -25,13 +35,13 @@ return require('packer').startup(function(use)
         "williamboman/mason-lspconfig.nvim",
         "neovim/nvim-lspconfig",
     }
---    use {
---        'nvim-treesitter/nvim-treesitter',
---        run = function()
---            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
---            ts_update()
---        end,
---    }
+    --    use {
+    --        'nvim-treesitter/nvim-treesitter',
+    --        run = function()
+    --            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+    --            ts_update()
+    --        end,
+    --    }
     use 'simrat39/symbols-outline.nvim'
     use {
         'stevearc/aerial.nvim',
@@ -40,5 +50,10 @@ return require('packer').startup(function(use)
     use "jbyuki/venn.nvim"
     use { 'akinsho/bufferline.nvim', tag = "v4.*", requires = 'nvim-tree/nvim-web-devicons' }
 
+    use 'preservim/vim-markdown'
     -- use "~/work/jin"
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
 end)
